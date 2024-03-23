72f91915aa9a63d3429b47dbc2e6c6771e21a42d
冯文山 <424433081@qq.com>
2024-03-23 11:58:41 +0800

    first commit

---
+++ lesson001_db/src/main/java/com/wenshan/Main.java
@@ -0,0 +1,14 @@
+package com.wenshan;
+
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Jdbc create = new Jdbc();
+
+        // create.create();
+        System.out.println("Hello world!");
+
+    }
+}
